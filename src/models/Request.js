34a846e99d323ef57250017c277ff1ff5ecc6b6f
Jamila <j.r.cba@hotmail.com>
2024-02-27 18:18:01 +0300

    modelfixed

--- src/models/Request.js
+++ src/models/Request.js
@@ -3,7 +3,7 @@ const { model, Schema } = require("mongoose");
 const RequestSchema = new Schema(
   {
     case: { type: String, required: true },
-    user: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    user: { type: Schema.Types.ObjectId, ref: "User" },
     location: { type: String, required: true },
     helper: { type: Schema.Types.ObjectId, ref: "Helper" },
     status: { type: String, enum: ["close", "open"] },
