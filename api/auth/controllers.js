8d9340b6b1103ec4947b1affbb8975a48d4d909a
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2024-02-27 20:40:25 +0300

    location update done and image for helper

--- api/auth/controllers.js
+++ api/auth/controllers.js
@@ -5,6 +5,8 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../../models/User");
+const Helper = require("../../models/Helper");
+const { response } = require("express");
 require("dotenv").config();
 
 const fetchUser = async (userId, next) => {
@@ -46,6 +48,13 @@ const register = async (req, res, next) => {
     const hashedPassword = await hashPassword(password);
     req.body.password = hashedPassword;
     const newUser = await User.create(req.body); // user
+    if (req.file) {
+      req.body.image = req.file.path;
+      req.body.user = newUser._id;
+      const helper = await Helper.create(req.body);
+      await newUser.updateOne({ helper: helper });
+    }
+
     const token = generateToken(newUser);
 
     res.status(201).json({ token: token });
@@ -72,6 +81,74 @@ const getMyProfile = async (req, res, next) => {
     next(error);
   }
 };
-module.exports = { register, login, getMyProfile, fetchUser };
+
+const findNearestRequest = async (req, res, next) => {
+  //   const helper = await Helper.findOneAndUpdate({ user: userId });
+
+  try {
+    Helper.find({
+      location: {
+        $near: {
+          $maxDistance: 1000,
+          $geometry: {
+            type: "Point",
+            coordinates: [long, latt],
+          },
+        },
+      },
+    }).find((error, results) => {
+      if (error) console.log(error);
+      console.log(JSON.stringify(results, 0, 2));
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+// const updateLocation = async (req, res, next) => {
+//   try {
+//     const user = req.user;
+//     const helper = await Helper.findOne({ user: user._id });
+//     if (!helper) {
+//       return res.status(404).send("Helper not found");
+//     }
+//     await helper.updateOne({
+//       location: { ...helper.location, coordinates: req.body.coordinates },
+//     });
+//     return res.status(204).end;
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+const updateLocation = async (req, res, next) => {
+  try {
+    const userId = req.user._id;
+
+    //const userId = req.body.userId; // Assuming the client sends userId directly
+    if (!userId) {
+      return res.status(400).send("User ID is required");
+    }
+    const helper = await Helper.findOneAndUpdate({ user: userId });
+    if (!helper) {
+      return res.status(404).send("Helper not found");
+    }
+    await helper.updateOne({
+      location: { type: "Point", coordinates: req.body.coordinates },
+    });
+    console.log(helper);
+    return res.status(204).end(); // Make sure to call end() as a function
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  register,
+  login,
+  getMyProfile,
+  fetchUser,
+  updateLocation,
+  findNearestRequest,
+};
 
 ////
--- models/Request.js
+++ models/Request.js
@@ -5,9 +5,13 @@ const { model, Schema } = require("mongoose");
 const RequestSchema = new Schema({
   case: { type: String, required: true },
   user: [{ type: Schema.Types.ObjectId, ref: "Skill" }],
-  location: { type: String, required: true },
+  location: {
+    type: { type: String },
+    coordinates: [],
+  },
   helper: { type: Schema.Types.ObjectId, ref: "Helper" },
 });
+RequestSchema.index({ location: "2dsphere" });
 
 module.exports = model("Request", RequestSchema);
 
