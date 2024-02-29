fe3178fc3ff2a36731bc8c65e1eddc011efed206
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2024-02-29 19:46:19 +0300

    find nearest request for helper done!

--- api/auth/controllers.js
+++ api/auth/controllers.js
@@ -10,6 +10,7 @@ const Request = require("../../models/Request");
 
 const Helper = require("../../models/Helper");
 const { response } = require("express");
+const { default: mongoose } = require("mongoose");
 
 require("dotenv").config();
 
@@ -93,14 +94,37 @@ const getMyProfile = async (req, res, next) => {
 //     await Request.findByIdAndUpdate(_id, req.body);
 //     res.status(204).end();
 
-const findNearestRequest = async (req, res, next) => {
+const findNearestRequestForHelper = async (req, res, next) => {
   try {
+    // Directly access helperId without using await
+    const helperId = req.user._id;
+    console.log("Helper ID:", helperId);
+
+    // Validate helperId before proceeding
+    if (!mongoose.Types.ObjectId.isValid(helperId)) {
+      return res.status(400).json({ message: "Invalid helper ID" });
+    }
+
+    // Proceed with finding the helper
+    const { helper } = await User.findById(helperId).populate("helper");
+    console.log("Helper:", helper);
+
+    if (!helper) {
+      return res.status(404).json({ message: "Helper not found" });
+    }
+
+    if (!helper.location || !helper.location.coordinates) {
+      return res.status(404).json({ message: "Helper's location not set" });
+    }
+
+    const [longitude, latitude] = helper.location.coordinates;
+
     const results = await Request.find({
       location: {
         $nearSphere: {
           $geometry: {
             type: "Point",
-            coordinates: [-112.110492, -36.09894999997],
+            coordinates: [longitude, latitude],
           },
           $minDistance: 0,
           $maxDistance: 100000, // Example: 100 kilometers
@@ -108,17 +132,40 @@ const findNearestRequest = async (req, res, next) => {
       },
     });
 
-    // Process results here
-    return res.json(results); // Send results to the client
+    return res.json(results);
   } catch (error) {
-    console.error(error);
+    console.error("Error:", error);
     next(error);
   }
 };
 
+// const findNearestRequest = async (req, res, next) => {
+//   try {
+//     const results = await Request.find({
+//       location: {
+//         $nearSphere: {
+//           $geometry: {
+//             type: "Point",
+//             coordinates: [-112.110492, -36.09894999997],
+//           },
+//           $minDistance: 0,
+//           $maxDistance: 100000, // Example: 100 kilometers
+//         },
+//       },
+//     });
+
+//     // Process results here
+//     return res.json(results); // Send results to the client
+//   } catch (error) {
+//     console.error(error);
+//     next(error);
+//   }
+// };
+
 const getHelperById = async (req, res, next) => {
   try {
     const helper = await Helper.findById(req.params._id);
+
     res.status(200).json(helper);
   } catch (error) {
     next(error);
@@ -243,7 +290,8 @@ module.exports = {
   fetchUser,
   updateLocation,
   assignRequest,
-  findNearestRequest,
+  //findNearestRequest,
+  findNearestRequestForHelper,
   getHelperById,
   updateHelperLocation,
 };
--- models/Request.js
+++ models/Request.js
@@ -1,22 +1,29 @@
 const { model, Schema } = require("mongoose");
 
-const RequestSchema = new Schema({
-  case: { type: String, required: true },
-  user: [{ type: Schema.Types.ObjectId, ref: "Skill" }],
-  location: {
-    type: {
-      type: String, // Don't do `{ location: { type: String } }`
-      enum: ["Point"], // 'location.type' must be 'Point'
-      // required: true,
+const RequestSchema = new Schema(
+  {
+    case: { type: String, required: true },
+    user: [{ type: Schema.Types.ObjectId, ref: "Skill" }],
+    location: {
+      type: {
+        type: String, // Don't do `{ location: { type: String } }`
+        enum: ["Point"], // 'location.type' must be 'Point'
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
-    coordinates: {
-      type: [Number],
-      // required: true,
+    helper: { type: Schema.Types.ObjectId, ref: "Helper" },
+    status: {
+      type: String,
+      enum: ["close", "open", "ongoing"],
+      default: "open",
     },
   },
-  helper: { type: Schema.Types.ObjectId, ref: "Helper" },
-  status: { type: String, enum: ["close", "open"] },
-});
+  { timestamps: true }
+);
 RequestSchema.index({ location: "2dsphere" });
 
 module.exports = model("Request", RequestSchema);
